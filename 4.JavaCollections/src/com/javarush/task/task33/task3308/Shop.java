d40add37286a6678f31cdc9044ca1b968a3c8fe2
DmRG <dmruslan@gmail.com>
2017-05-06 00:08:00 +0300

    06.05.2017

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task33.task3308;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.List;
+
+/**
+ * Created by DmRG on 05.05.2017.
+ */
+
+@XmlType(name = "shop")
+@XmlRootElement
+public class Shop {
+    public Goods goods;
+    public int count;
+    public double profit;
+    public String[] secretData;
+
+
+
+    public static class Goods {
+        @XmlElement(name="names")
+        List<String> names;
+    }
+}
